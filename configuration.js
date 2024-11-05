48c240bf2349342e15a1748ee875076ceb8fbd6f
lucas-lopez-dovina <lopz.l123@gmail.com>
2024-11-05 15:46:38 +0100

    Update configuration.js

--- configuration.js
+++ configuration.js
@@ -3,7 +3,10 @@ const config = {
   description: {
     FR: {
       short: `Lucas Lopez Dovina est un chercheur en sciences politiques et réalise, depuis octobre 2024, une thèse au sein du <a
-          href="https://umr-lisis.fr/membre/lucas-lopez-dovina/" target="_blank">Laboratoire Interdisciplinaire Sciences Innovations Sociétes (LISIS)</a>. Il travaille sur les organisations finançant des initiatives de restauration écologique / renaturation dans l’action publique territoriale, sous la direction de Gabrielle Bouleau (INRAE/LISIS). La thèse est financée par un contrat doctoral de l’Université Gustave Eiffel.`,
+          href="https://umr-lisis.fr/membre/lucas-lopez-dovina/" target="_blank">Laboratoire Interdisciplinaire Sciences Innovations Sociétes (LISIS)</a>. Il travaille sur les organisations finançant des initiatives de restauration écologique / renaturation dans l’action publique territoriale, sous la direction de Gabrielle Bouleau (INRAE/LISIS). La thèse est financée par un contrat doctoral de l’Université Gustave Eiffel.
+          Après avoir été diplômé du master « Transitions Ecologiques » de Sciences Po Grenoble – au cours duquel il a réalisé un stage au LESSEM (un laboratoire INRAE, Grenoble) sur la gouvernance du bois-bûche en Chartreuse (publication en cours) – il a réalisé une année supplémentaire au sein du master « Etudes Environnementales » à l’EHESS.
+
+Son travail s’inscrit entre la sociologie de l’action publique, les « street-level bureaucrats » et les politiques de biodiversité.`,
       long: "",
     },
   },
