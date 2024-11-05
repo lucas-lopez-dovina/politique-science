25680d91e908c98ab3015d032c6d23499267d15d
lucas-lopez-dovina <lopz.l123@gmail.com>
2024-11-05 15:44:30 +0100

    Update configuration.js

--- configuration.js
+++ configuration.js
@@ -3,7 +3,7 @@ const config = {
   description: {
     FR: {
       short: `Lucas Lopez Dovina est un chercheur en sciences politiques et réalise, depuis octobre 2024, une thèse au sein du <a
-          href="https://umr-lisis.fr/" target="_blank">Laboratoire Interdisciplinaire Sciences Innovations Sociétes (LISIS)</a>. Il travaille sur les organisations finançant des initiatives de restauration écologique / renaturation dans l’action publique territoriale, sous la direction de Gabrielle Bouleau (INRAE/LISIS). La thèse est financée par un contrat doctoral de l’Université Gustave Eiffel.`,
+          href="https://umr-lisis.fr/membre/lucas-lopez-dovina/" target="_blank">Laboratoire Interdisciplinaire Sciences Innovations Sociétes (LISIS)</a>. Il travaille sur les organisations finançant des initiatives de restauration écologique / renaturation dans l’action publique territoriale, sous la direction de Gabrielle Bouleau (INRAE/LISIS). La thèse est financée par un contrat doctoral de l’Université Gustave Eiffel.`,
       long: "",
     },
   },
